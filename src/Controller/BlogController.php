e5f8befd9eadd016feef17fa110a6b7a67cc226d
mathieu <drabland@gmail.com>
2019-05-15 14:26:33 +0200

    quete 8

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,95 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog_index")
+     */
+    public function index(): Response
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles]
+        );
+    }
+
+    /**
+     * @Route("/blog/show/{slug<^[a-z0-9-]+$>?}", name="blog_show")
+     */
+    public function show(?string $slug) : Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with '.$slug.' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug,
+            ]
+        );
+    }
+    /**
+     * @Route("blog/category/{categoryName}", name="blog_category")
+     */
+    public function showByCategory(string $categoryName) :Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneByName($categoryName);
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'Category not found in category-s table'
+            );
+        }
+        $articles = $category->getArticles();
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No articles found for ' . $categoryName . ' in article\'s table'
+            );
+        }
+        return $this->render(
+            'blog/category.html.twig',
+            [
+                'category' => $categoryName,
+                'articles' => $articles
+            ]
+        );
+    }
+}
