2a2d74f32b74270001301d7e9956da66a38b35a6
mathieu <drabland@gmail.com>
2019-05-29 15:10:34 +0200

    fixtures

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Article;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Repository\ArticleRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use  Faker;
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Faker\Factory::create('fr_FR');
+        for ($i = 0; $i<=50; $i++) {
+            $article = new Article();
+            $article->setTitle(mb_strtolower($faker->title));
+            $article->setContent(mb_strtolower($faker->text));
+            $article->setCategory($this->getReference('category_'.random_int(0, 5)));
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+}
